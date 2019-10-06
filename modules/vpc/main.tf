76b72c7db76efe7bb2908258e1ad9aae1ee944c8
Alfonso Vasquez <alfonso.vasquez@craftersoftware.com>
2019-10-06 14:27:23 -0400

    Fixed wrong public_subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -40,7 +40,7 @@ module "vpc" {
   cidr               = var.vpc_cidr
   azs                = var.azs
   private_subnets    = var.private_subnets_cidrs
-  public_subnets     = var.private_subnets_cidrs
+  public_subnets     = var.public_subnets_cidrs
   enable_nat_gateway = true
   single_nat_gateway = true
 
